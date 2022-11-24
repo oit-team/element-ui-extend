377e5dee0426c6ee0c05af26e60e22e4e53937a2
邱旭 <uxuip@outlook.com>
2022-11-24 15:05:21 +0800

    feat: export SearchForm

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import TablePage from './src/TablePage'
+import SearchForm from './src/SearchForm'
 import Input from './src/Input'
 import Upload from './src/Upload'
 
 export {
   TablePage,
+  SearchForm,
   Input,
   Upload,
 }
